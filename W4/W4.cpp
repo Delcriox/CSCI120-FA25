4965f82f8bc30d232e1129795c509f061c82ae6e
Delcriox <sle004@student.butte.edu>
2025-09-16 20:24:28 +0000

    something

---
+++ W4/W4.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main() {
+    int x = 35;
+    bool b = true; // boolean value
+    std::printf("%i", b); // booleans are numbers. *Mind blown*
+    std::printf("%i", x);
+    std::printf("hello world\n");
+    return 0;
+}
